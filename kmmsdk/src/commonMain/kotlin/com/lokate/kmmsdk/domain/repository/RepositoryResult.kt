8d981a3b141e95fc479730345f5b1b65f5a234e1
Umut Baser <umutbaser01@icloud.com>
2024-03-07 16:12:50 +0300

    refactor structure

--- kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/domain/repository/RepositoryResult.kt
+++ kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/domain/repository/RepositoryResult.kt
@@ -1,10 +1,19 @@
 package com.lokate.kmmsdk.domain.repository
 
+import com.lokate.kmmsdk.data.datasource.DSResult
+
 sealed class RepositoryResult<out T> {
-    data class Success<T>(val body: T) : RepositoryResult<T>()
+    data class Success<out T>(val body: T) : RepositoryResult<T>()
 
     data class Error(
-        val message: String?,
-        val errorMessage: String?,
+        val message: String,
+        val errorType: Any
     ) : RepositoryResult<Nothing>()
+}
+
+fun <T> DSResult<T>.toRepositoryResult(): RepositoryResult<T> {
+    return when (this) {
+        is DSResult.Success -> RepositoryResult.Success(data)
+        is DSResult.Error -> RepositoryResult.Error(message.orEmpty(), errorType)
+    }
 }
