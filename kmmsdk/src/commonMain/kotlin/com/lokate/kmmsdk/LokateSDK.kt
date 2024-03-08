86a34b526b785cabd17a2b951bee93bdc96f065b
Umut Baser <umutbaser01@icloud.com>
2024-03-08 03:46:20 +0300

    add event send logic

--- kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/LokateSDK.kt
+++ kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/LokateSDK.kt
@@ -2,7 +2,22 @@ package com.lokate.kmmsdk
 
 import com.lokate.kmmsdk.Defaults.DEFAULT_BEACONS
 import com.lokate.kmmsdk.Defaults.GONE_CHECK_INTERVAL
+import com.lokate.kmmsdk.data.datasource.local.authentication.AuthenticationLocalDS
+import com.lokate.kmmsdk.data.datasource.local.beacon.BeaconLocalDS
+import com.lokate.kmmsdk.data.datasource.local.factory.getDatabase
+import com.lokate.kmmsdk.data.datasource.remote.authentication.AuthenticationAPI
+import com.lokate.kmmsdk.data.datasource.remote.authentication.AuthenticationRemoteDS
+import com.lokate.kmmsdk.data.datasource.remote.beacon.BeaconAPI
+import com.lokate.kmmsdk.data.datasource.remote.beacon.BeaconRemoteDS
+import com.lokate.kmmsdk.data.repository.AuthenticationRepositoryImpl
+import com.lokate.kmmsdk.data.repository.BeaconRepositoryImpl
+import com.lokate.kmmsdk.domain.model.beacon.ActiveBeacon
 import com.lokate.kmmsdk.domain.model.beacon.BeaconScanResult
+import com.lokate.kmmsdk.domain.model.beacon.EventRequest
+import com.lokate.kmmsdk.domain.model.beacon.EventStatus
+import com.lokate.kmmsdk.domain.repository.RepositoryResult
+import com.lokate.kmmsdk.utils.extension.EMPTY_STRING
+import com.russhwolf.settings.Settings
 import io.ktor.util.date.getTimeMillis
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,7 +36,19 @@ class LokateSDK {
         val log = logging("LokateSDK")
     }
 
+    //move to DI
     private val beaconScanner = getBeaconScanner()
+    private val authenticationRepository = AuthenticationRepositoryImpl(
+        AuthenticationRemoteDS(AuthenticationAPI()),
+        AuthenticationLocalDS(Settings())
+    )
+    private val beaconRepository = BeaconRepositoryImpl(
+        authenticationRepository = authenticationRepository,
+        remoteDS = BeaconRemoteDS(BeaconAPI()),
+        localDS = BeaconLocalDS(getDatabase())
+    )
+
+    private val branchBeacons = mutableListOf<ActiveBeacon>()
 
     private val lokateJob = SupervisorJob()
     private val lokateScope = CoroutineScope(Dispatchers.IO + lokateJob)
@@ -31,23 +58,78 @@ class LokateSDK {
     private val newComer = MutableSharedFlow<BeaconScanResult>()
     private val alreadyIn = MutableSharedFlow<BeaconScanResult>()
     private val gone = MutableSharedFlow<BeaconScanResult>()
+    private var appTokenSet: Boolean = false
 
     fun getScanResultFlow(): Flow<BeaconScanResult> {
         return beaconScanner.scanResultFlow()
     }
 
+    fun setAppToken(appToken: String) {
+        lokateScope.launch {
+            authenticationRepository.setAppToken(appToken)
+        }.invokeOnCompletion {
+            log.d { "App token set" }
+            appTokenSet = true
+        }
+    }
+
+    fun fetchActiveBeacons(branchId: String) {
+        val branch = branchId.ifEmpty {
+            log.e { "Branch ID is empty" }
+            "1b224840-de56-41a2-92e0-959193b0035e"
+        }
+        lokateScope.launch {
+            beaconRepository.fetchBeacons(branch).let {
+                if (it is RepositoryResult.Success) {
+                    branchBeacons.addAll(it.body)
+                    log.d { "Branch beacons: $branchBeacons" }
+                }
+            }
+        }
+    }
+
+    private fun checkAppToken() {
+        lokateScope.launch {
+            authenticationRepository.getAppToken().let {
+                if (it is RepositoryResult.Success && it.body.isNotEmpty()) {
+                    appTokenSet = true
+                }
+            }
+        }
+    }
+
+    init {
+        log.d { "LokateSDK initializing" }
+        // check if app token is set
+        checkAppToken()
+
+        if (!appTokenSet) {
+            log.e { "App token not set" }
+            // use default token
+            setAppToken("1")
+        }
+    }
+
     fun startScanning() {
-        log.d { "SA" }
         beaconScanner.setRegions(DEFAULT_BEACONS)
+        //
+        fetchActiveBeacons(EMPTY_STRING)
+        //
         beaconScanner.startScanning()
 
         scanResultHandler(
             beaconScannerFlow =
-                beaconScanner.scanResultFlow().transform { scan ->
-                    if (scan.accuracy != null && scan.accuracy > -1.0) {
-                        emit(scan)
-                    }
-                },
+            beaconScanner.scanResultFlow().transform { scan ->
+                val beacon = branchBeacons.firstOrNull {
+                    it.id.lowercase() == scan.beaconUUID.lowercase() &&
+                            it.major == scan.major.toString() &&
+                            it.minor == scan.minor.toString()
+                }
+                val comparison = (beacon == null || beacon.range.ordinal >= scan.proximity.ordinal)
+                if (scan.accuracy != null && scan.accuracy > -1.0 && comparison) {
+                    emit(scan)
+                }
+            },
         )
         checkGone()
         flowEmitter()
@@ -74,16 +156,46 @@ class LokateSDK {
         lokateScope.launch {
             newComer.collect {
                 log.d { "Newcomer: $it" }
+                beaconRepository.sendBeaconEvent(
+                    EventRequest(
+                        "umut",
+                        it.beaconUUID,
+                        it.major.toString(),
+                        it.minor.toString(),
+                        EventStatus.ENTER,
+                        it.seen
+                    )
+                ).also { log.d { "Send newcomer in: $it" } }
             }
         }
         lokateScope.launch {
             alreadyIn.collect {
                 log.d { "Already in: $it" }
+                beaconRepository.sendBeaconEvent(
+                    EventRequest(
+                        "umut",
+                        it.beaconUUID,
+                        it.major.toString(),
+                        it.minor.toString(),
+                        EventStatus.STAY,
+                        it.seen
+                    )
+                ).also { log.d { "Send already in: $it" } }
             }
         }
         lokateScope.launch {
             gone.collect {
                 log.d { "Gone: $it" }
+                beaconRepository.sendBeaconEvent(
+                    EventRequest(
+                        "umut",
+                        it.beaconUUID,
+                        it.major.toString(),
+                        it.minor.toString(),
+                        EventStatus.EXIT,
+                        it.seen
+                    )
+                ).also { log.d { "Send gone: $it" } }
             }
         }
     }
@@ -93,8 +205,8 @@ class LokateSDK {
             beaconScannerFlow.collect { scan ->
                 activeBeacons.firstOrNull {
                     it.beaconUUID == it.beaconUUID &&
-                        it.major == it.major &&
-                        it.minor == it.minor
+                            it.major == it.major &&
+                            it.minor == it.minor
                 }
                     ?.let {
                         alreadyIn.emit(it.copy(seen = getTimeMillis()))
