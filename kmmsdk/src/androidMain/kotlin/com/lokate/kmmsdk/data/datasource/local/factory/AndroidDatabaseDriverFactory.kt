c360d888fc6745ed957f39040b96893ff0142376
Umut Baser <umutbaser01@icloud.com>
2024-03-08 03:44:35 +0300

    fix db access bug

--- kmmsdk/src/androidMain/kotlin/com/lokate/kmmsdk/data/datasource/local/factory/AndroidDatabaseDriverFactory.kt
+++ kmmsdk/src/androidMain/kotlin/com/lokate/kmmsdk/data/datasource/local/factory/AndroidDatabaseDriverFactory.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.lokate.kmmsdk.Database
+import com.lokate.kmmsdk.applicationContext
 
-actual class DatabaseDriverFactory(private val context: Context) {
-    actual fun createDriver(): SqlDriver {
+class AndroidDatabaseDriverFactory(private val context: Context) : DatabaseDriverFactory {
+    override fun createDriver(): SqlDriver {
         return AndroidSqliteDriver(Database.Schema, context, "test.db")
     }
 }
+
+actual fun getDatabase(): Database = createDatabase(AndroidDatabaseDriverFactory(applicationContext))
