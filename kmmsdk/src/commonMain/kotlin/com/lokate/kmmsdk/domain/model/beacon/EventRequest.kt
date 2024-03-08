8428b9ac9fdaa6202fad11b2de87994ea8e0becb
Umut Baser <umutbaser01@icloud.com>
2024-03-08 03:44:46 +0300

    update event request

--- kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/domain/model/beacon/EventRequest.kt
+++ kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/domain/model/beacon/EventRequest.kt
@@ -1,8 +1,26 @@
 package com.lokate.kmmsdk.domain.model.beacon
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class EventRequest(
     val customerId: String,
     val beaconUID: String,
-    val status: String,
+    val major: String,
+    val minor: String,
+    val status: EventStatus,
     val timestamp: Long,
 )
+
+@Serializable
+enum class EventStatus {
+    @SerialName("ENTER")
+    ENTER,
+
+    @SerialName("EXIT")
+    EXIT,
+
+    @SerialName("STAY")
+    STAY;
+}
