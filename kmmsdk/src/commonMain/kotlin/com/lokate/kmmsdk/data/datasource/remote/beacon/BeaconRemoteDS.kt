25419badc4a6220783299533b4bef4b970e8b018
Umut Baser <umutbaser01@icloud.com>
2024-03-08 03:48:13 +0300

    remove unnecessary debug comments

--- kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/data/datasource/remote/beacon/BeaconRemoteDS.kt
+++ kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/data/datasource/remote/beacon/BeaconRemoteDS.kt
@@ -6,13 +6,11 @@ import com.lokate.kmmsdk.data.datasource.remote.ApiResponse
 import com.lokate.kmmsdk.data.datasource.toDSResult
 import com.lokate.kmmsdk.domain.model.beacon.ActiveBeacon
 import com.lokate.kmmsdk.domain.model.beacon.EventRequest
-import org.lighthousegames.logging.logging
 
 class BeaconRemoteDS(private val api: BeaconAPI) : BeaconDS {
     override suspend fun fetchBeacons(branchId: String): DSResult<List<ActiveBeacon>> {
         println()
         val a = api.getActiveBeacons(branchId)
-        logging().d { a }
         return a.toDSResult()
     }
 
@@ -21,7 +19,6 @@ class BeaconRemoteDS(private val api: BeaconAPI) : BeaconDS {
             if (it is ApiResponse.Success) {
                 return DSResult.Success(true)
             } else {
-                logging("saaaaa").e { it }
                 return DSResult.Error("Couldn't send beacon event!", "No response!")
             }
         }
