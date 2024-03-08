ef41b7e1d8bb2674fd77cd39bcc55e3d2774cd2e
Umut Baser <umutbaser01@icloud.com>
2024-03-08 03:44:58 +0300

    increase gone timeout

--- kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/Defaults.kt
+++ kmmsdk/src/commonMain/kotlin/com/lokate/kmmsdk/Defaults.kt
@@ -5,7 +5,7 @@ import com.lokate.kmmsdk.domain.model.beacon.LokateBeacon
 
 @Suppress("MagicNumber")
 object Defaults {
-    const val DEFAULT_TIMEOUT_BEFORE_GONE = 3000L
+    const val DEFAULT_TIMEOUT_BEFORE_GONE = 10000L
     const val GONE_CHECK_INTERVAL = 5000L
     const val BEACON_LAYOUT_IBEACON = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25,i:0-56"
     val DEFAULT_BEACONS =
