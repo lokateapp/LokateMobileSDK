4c33ac2aa254b303ec9948712642c2d1664c678d
inceisciberkay <inceisciberkay@gmail.com>
2024-04-23 21:21:18 +0300

    add notification pool for camapigns

---
+++ composeApp/src/commonMain/kotlin/com.lokate.demo/market/Mappings.kt
@@ -0,0 +1,24 @@
+package com.lokate.demo.market
+
+val notificationPool = mapOf(
+    "Campaign 1" to listOf(
+        "There is a discount on \"Head & Shoulders Mentol 400ML\"",
+        "Buy 2, get 1 free on Dove shampoo",
+        "Save 30% on L'Oreal hair care products"
+    ),
+    "Campaign 2" to listOf(
+        "Special offer on electronics this weekend",
+        "Get a free wireless charger with any smartphone purchase",
+        "Limited stock: 50% off on selected laptops"
+    ),
+    "Campaign 3" to listOf(
+        "Get 20% off on all clothing items",
+        "New arrivals: Explore our latest fashion collection",
+        "Buy one dress, get the second one at half price"
+    ),
+    "Campaign 4" to listOf(
+        "Limited time offer: Buy one get one free on selected items",
+        "Flash sale: Up to 50% off on home appliances",
+        "Exclusive deals for loyalty members only"
+    )
+)
